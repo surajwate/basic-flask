1680bef8d7e9da0292d2505cdffc9c92c024d2d7
Suraj Wate <suraj.wate@gmail.com>
2021-03-08 15:22:59 +0530

    get and post in single route

--- app.py
+++ app.py
@@ -26,22 +26,28 @@ def query():
     return "<h1>Hell {} from {}. You are on query page.</h1>".format(name, location)
 
 
-@app.route('/theform')
+@app.route('/theform', methods=['GET', 'POST'])
 def theform():
-    return """
-            <form method="POST" action='/process'>
-                <input type="text" name="name">
-                <input type="text" name="location">
-                <input type="submit" value="Submit">
-            </form>
-    """
-
+    if request.method == 'GET':
+        return """
+                <form method="POST" action='/theform'>
+                    <input type="text" name="name">
+                    <input type="text" name="location">
+                    <input type="submit" value="Submit">
+                </form>
+        """
+    else:
+        name = request.form['name']
+        location = request.form['location']
+        return "<h1>Hello {} from {}. You have successfully submitted the form.</h1>".format(name, location)
+    
+"""
 @app.route('/process', methods=['POST'])
 def process():
     name = request.form['name']
     location = request.form['location']
     return "<h1>Hello {} from {}. You have successfully submitted the form.</h1>".format(name, location)
-
+"""
 @app.route('/jsonprocess', methods=['POST'])
 def jsonprocess():
     data = request.get_json()
